565edc99763e02431b2f0eea2d814342208d69df
HTselenium <124279315+HTselenium@users.noreply.github.com>
2024-02-21 11:25:44 +0800

    Create index.js
    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Importing http module
+const http = require('http');
+
+// Creating server
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+
+// Defining port
+const port = 3000;
+
+// Server listens on defined port
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+})
